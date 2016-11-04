ddd753e9739a228f01185c24aee6baffac4788ce
Cesar Varela <mail@cesarvarela.com>
2016-11-03 22:42:34 -0300

    Add font awesome

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom'
 
 import { App } from "./App"
 
+require('../node_modules/font-awesome/scss/font-awesome.scss')
 require("./sass/main.scss")
+
 
 ReactDOM.render(<App />, document.getElementById("example"));
