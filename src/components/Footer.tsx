390e662fe17e7fec9be2c3240ea48df4670bc139
Cesar Varela <mail@cesarvarela.com>
2016-11-04 02:55:02 -0300

    Update footer
    And some cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ export class Footer extends React.Component<{}, {}> {
 
     render() {
         return <footer>
-            <p className="copy">© 2016 - Cesar Varela</p>
+            <p>Built using TypeScript, REACT, Webpack, SASS, etc. and the beautiful VSCode
+            <br />
+            You can check the source <a href="https://github.com/cesarvarela/cesarvarela.github.io/tree/src" target="_blank">here</a></p>
+            <p className="text-muted">© 2016 Cesar Varela</p>
         </footer>
     }
 }
