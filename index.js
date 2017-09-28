dd302e0cc48a2d0ab0f48a631710e109e6f158f6
Cesar Varela <mail@cesarvarela.com>
2017-09-28 16:24:16 -0300

    Updates

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App_1 = require("./App");
-require('../node_modules/font-awesome/scss/font-awesome.scss');
-require("./sass/main.scss");
-ReactDOM.render(React.createElement(App_1.App, null), document.getElementById("example"));
-//# sourceMappingURL=index.js.map
